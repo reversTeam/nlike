191ee06698b60be5e259a6970170fa3e27871b36
Riviere Théotime <theotimeriviere@gmail.com>
2017-01-07 15:45:47 +0100

    :typo: normalize output benchmark and compute global information

--- modules/echo/bundles/echo/client/tests/echo-client-test.go
+++ modules/echo/bundles/echo/client/tests/echo-client-test.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"github.com/reversTeam/nlike/modules/echo/bundles/echo/client"
 	"log"
 	"sync"
 	"time"
 )
 
+const (
+	name = "EchoClientBenchmark"
+)
+
+func getFlags() (log_level *int, nb_concurents *int, nb_requests *int) {
+	nb_concurents = flag.Int("concurents", 400, "Number of concourrents")
+	nb_requests = flag.Int("requests", 5000, "Number of requests")
+	log_level = flag.Int("log", 1, "Display log mode")
+
+	flag.Parse()
+	return
+}
+
 func main() {
-	nb_pistes := 400
-	nb_request := 2500
+	log_level, nb_concurents, nb_requests := getFlags()
+	total_requests := *nb_concurents * *nb_requests
+
+	log.Printf("[%s]: Start benchmark", name)
+	defer log.Printf("[%s]: End benchmark", name)
+	log.Printf("[%s]: intiialized with %d level logs", name, *log_level)
+	log.Printf("[%s]: initialized with %d concurents", name, *nb_concurents)
+	log.Printf("[%s]: initialized with %d requets", name, *nb_requests)
 	wg := sync.WaitGroup{}
 
 	c, err := client.NewClient()
@@ -20,15 +39,25 @@ func main() {
 	}
 
 	start := time.Now()
-	wg.Add(nb_pistes)
-	for i := 0; i < nb_pistes; i++ {
+	wg.Add(*nb_concurents)
+	for i := 0; i < *nb_concurents; i++ {
 		go func(c *client.Client, wg *sync.WaitGroup) {
 			defer wg.Done()
-			for j := 0; j < nb_request; j++ {
+			for j := 0; j < *nb_requests; j++ {
 				c.Echo("Hello world !")
 			}
 		}(c, &wg)
 	}
 	wg.Wait()
-	fmt.Println(time.Since(start))
+	past_time := time.Since(start)
+	past_time_in_nano := float64(past_time.Nanoseconds())
+	past_time_in_seconds := past_time_in_nano / 1000000000
+	requests_per_second := float64(total_requests) / past_time_in_seconds
+
+	avg_time_per_request := past_time_in_nano / float64(total_requests)
+
+	log.Printf("[%s]: proceded %d in %s", name, total_requests, past_time)
+	log.Printf("[%s]: result %.3f requests per seconds", name, requests_per_second)
+	log.Printf("[%s]: result %.0f avg nanoseconds per request", name, avg_time_per_request)
+
 }
